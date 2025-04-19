9b14956532d0a7ce567c95e1603c9ef33e0e8856
Delaporte Alexy <alexy.delaporte@etudiant.univ-lr.fr>
2025-04-19 18:01:38 +0200

    Add thumbnail focus on work page

--- portfolio/js/work.js
+++ portfolio/js/work.js
@@ -22,3 +22,32 @@ workMenuLinks.forEach((link, i) => {
     updateContent(workState, workContent, workMenuLinks, i);
   });
 });
+
+const black = document.querySelector(".black");
+const thumbnails = document.querySelectorAll(".thumbnail");
+
+const title = black.querySelector(".title");
+const iframe = black.querySelector("iframe");
+const description = black.querySelector(".video__description");
+const client = black.querySelectorAll(".info__text")[0];
+const fonction = black.querySelectorAll(".info__text")[1];
+const date = black.querySelectorAll(".info__text")[2];
+const closeBlack = black.querySelector(".black__close");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    title.textContent = thumbnail.dataset.title;
+    iframe.src = thumbnail.dataset.src;
+    description.textContent = thumbnail.dataset.description;
+    client.textContent = thumbnail.dataset.client;
+    fonction.textContent = thumbnail.dataset.fonction;
+    date.textContent = thumbnail.dataset.date;
+
+    black.classList.remove("hidden");
+  });
+});
+
+closeBlack.addEventListener("click", () => {
+  black.classList.add("hidden");
+  iframe.src = "";
+});
